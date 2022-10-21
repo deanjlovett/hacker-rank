ede42e5ab3514ba6d79c6caca3dd2c4ee3af3ec8
Dean Lovett <dean.lovett@gmail.com>
2022-10-20 23:10:02 -0700

    add init commit & js solution

---
+++ hacker-rank-pangrams/pangrams.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function pangrams(s) {
+    // Write your code here
+    
+    let myset = new Set( s.toLowerCase().split('') )
+    myset.delete(' ');
+    const size = myset.size;
+    let ret = 'pangram';
+    return size === 26 ? ret : 'not ' + ret;
+}
+
+function main() {
+    
+    const s = readLine();
+    const result = pangrams(s);
+    
+    if(process.env.OUTPUT_PATH) {
+        const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+        ws.write(result + '\n');
+        ws.end();
+    }else{
+        console.log(result);
+    }
+}
