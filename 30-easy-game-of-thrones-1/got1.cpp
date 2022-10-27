efeba3e1a3a58d554a24e3bf4a38a0b1c50a6a1a
Dean Lovett <dean.lovett@gmail.com>
2022-10-26 20:30:07 -0700

    add C++ solution

---
+++ 30-easy-game-of-thrones-1/got1.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+using namespace std;
+
+/*
+ * Complete the 'gameOfThrones' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string gameOfThrones(string s) {
+    unordered_map<char, unsigned int> map;
+    for(auto it = s.cbegin(); it != s.cend(); ++it){
+        if( map.find(*it) != map.end() ){
+            map[*it] = map[*it] + 1;
+        }else{
+            map[*it] = 1;
+        }
+    }
+    auto oddCount = 0;
+    for(auto it = map.begin(); it != map.end(); ++it){
+        if( (it->second % 2) != 0 && (++oddCount) > 1 ){
+            return "NO";
+        }    
+    }
+    return "YES";
+}
+
+int main()
+{
+
+    string s;
+    getline(cin, s);
+    
+    string result = gameOfThrones(s);
+
+    if( getenv("OUTPUT_PATH") ) {
+        ofstream fout(getenv("OUTPUT_PATH"));
+        fout << result << "\n";
+        fout.close();
+    }else{
+        cout << result << "\n";;
+    }
+
+    return 0;
+}
