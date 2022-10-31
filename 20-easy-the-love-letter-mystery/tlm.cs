15184bf8381940b35bff3a7c60205557a0e808b4
Dean Lovett <dean.lovett@gmail.com>
2022-10-30 20:03:12 -0700

    add solution in C++ & C#

---
+++ 20-easy-the-love-letter-mystery/tlm.cs
@@ -0,0 +1,69 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'theLoveLetterMystery' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int theLoveLetterMystery(string s)
+    {
+        int count = 0;
+        vector<char> arr(s.begin(), s.end());
+        int idx_b = arr.size()-1;
+        int idx_f = 0;
+        for(; idx_f < idx_b; ++idx_f, --idx_b ) {
+
+            char f = arr[idx_f]; // front
+            char b = arr[idx_b]; // back
+
+            if( f > b ){
+                count += f - b;
+            }
+            if( b > f ){
+                count += b - f;
+            }
+        }
+        return count;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = Result.theLoveLetterMystery(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
