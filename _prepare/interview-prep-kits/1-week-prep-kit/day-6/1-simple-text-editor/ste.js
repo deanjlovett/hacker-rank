6c9497bd743bd15c1640bf262665819c8ea40aed
Dean Lovett <dean.lovett@gmail.com>
2023-03-06 23:29:14 -0800

    add refined javascript solution

--- _prepare/interview-prep-kits/1-week-prep-kit/day-6/1-simple-text-editor/ste.js
+++ _prepare/interview-prep-kits/1-week-prep-kit/day-6/1-simple-text-editor/ste.js
@@ -7,47 +7,20 @@ function processData(input) {
   }
   
   let stack = [""];
-  
-  function my_append(s){
-      stack.push( last(stack) + s );
+  let map = {
+    1: my_append = (s)=>{stack.push(  last(stack) + s         )},
+    2: my_delete = (k)=>{stack.push(  last(stack).slice(0,-k) )},
+    3:  my_print = (k)=>{console.log( last(stack).charAt(k-1) )},
+    4:   my_undo = (_)=>{stack.pop()},
   };
-  function my_delete(k){
-      stack.push( last(stack).slice(0,-k) )  
-  }
-  function my_print(k){
-      console.log( last(stack).charAt(k-1));
-  }
-  function my_undo(dummy_ignore){
-      stack.pop();
-  }
-  // let map = {
-  //   1: my_append=(s)=>{stack.push( last(stack) + s )},
-  //   2: my_delete=(k)=>{stack.push( last(stack).slice(0,-k) )},
-  //   3:  my_print=(k)=>{console.log( last(stack).charAt(k-1));},
-  //   4:   my_undo=(_)=>{stack.pop()},
-  // };
-let map = {
-  0:  my_dummy,
-  1: my_append,
-  2: my_delete,
-  3:  my_print,
-  4:   my_undo,
-};
 
   let cmds = input.split('\n');
   let num_cmds = parseInt( cmds.shift()[0] );
 
   cmds.forEach((e,idx)=>{
       e = e.split(' ');
-      if( e.length<2) e.push("dummy");
-      
+      if( e.length<2) e.push("dummy ignore");
       // map[ e[0] ]( e[1] );
-      
-      if( ! map.hasOwnProperty( e[0] ) ) {
-          // we should complain
-          return;
-      }
-      
       let my_func = map[ e[0] ];
       my_func( e[1] );
   });
